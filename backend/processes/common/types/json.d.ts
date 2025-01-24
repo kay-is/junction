f0f2a28e5d2b82b1f73010a13d11d7e6a159ad50
K <kay-is@users.noreply.github.com>
2025-01-24 12:05:02 +0000

    chore: factor out AO types into lib

--- backend/processes/common/types/json.d.ts
+++
@@ -1,5 +0,0 @@
-/** @noResolution */
-declare module "json" {
-  function decode<ReturnType>(this: void, jsonString: string): ReturnType
-  function encode(this: void, data: unknown): string
-}
--- backend/processes/common/types/pretty.d.ts
+++
@@ -1,4 +0,0 @@
-/** @noResolution */
-declare module ".pretty" {
-  function tprint(this: void, data: unknown, indent?: number): string
-}
